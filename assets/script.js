d296785b91354ddf22d477f54a1cc143cb4f6913
Music Box <latuanja.c@gmail.com>
2020-12-19 21:33:09 -0600

    Added weather icon, added styling to the icons, added variables for ccities and displays, added API key

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var city = "St. Louis";
+var lat = "";
+var lng = "";
+
+var APIKey = "dd1b0371c21fc82ebaf3890f20045d6c";
+
+var forecastDisplay = $("#five-day-display");
+var currentWeatherDisplay = $("#display-current-weather");
+
+var savedCitiesArray = ["St. Louis"];
+
+var now = moment();
